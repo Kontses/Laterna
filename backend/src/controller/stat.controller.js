a893211fddc72b106f5161aebb80802d2151eeef
kontses <libys97@gmail.com>
2025-05-20 10:42:30 +0300

    Refactor getStats function to simplify artist count retrieval and improve performance

--- backend/src/controller/stat.controller.js
+++ backend/src/controller/stat.controller.js
@@ -1,39 +1,26 @@
 import { Album } from "../models/album.model.js";
 import { Song } from "../models/song.model.js";
 import { User } from "../models/user.model.js";
+import { Artist } from "../models/artist.model.js"; // Import Artist model
 
 export const getStats = async (req, res, next) => {
 	try {
-		const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
+		const [totalSongs, totalAlbums, totalUsers, totalArtists] = await Promise.all([
 			Song.countDocuments(),
 			Album.countDocuments(),
 			User.countDocuments(),
 
-			Song.aggregate([
-				{
-					$unionWith: {
-						coll: "albums",
-						pipeline: [],
-					},
-				},
-				{
-					$group: {
-						_id: "$artist",
-					},
-				},
-				{
-					$count: "count",
-				},
-			]),
+			Artist.countDocuments(),
 		]);
 
 		res.status(200).json({
 			totalAlbums,
 			totalSongs,
 			totalUsers,
-			totalArtists: uniqueArtists[0]?.count || 0,
+			totalArtists,
 		});
 	} catch (error) {
+		console.error("Error fetching stats:", error); // Log the error details
 		next(error);
 	}
 };
