d7126220d7f0575e131f8a887c12c568018721a0
kontses <libys97@gmail.com>
2025-05-23 14:16:31 +0300

    Στο HomePage δημιουργήθηκε Function καλωσορίσματος

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,9 +1,10 @@
 import { useMusicStore } from "@/stores/useMusicStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import FeaturedSection from "./components/FeaturedSection";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import SectionGrid from "./components/SectionGrid";
 import { usePlayerStore } from "@/stores/usePlayerStore";
+import { useUser } from "@clerk/clerk-react";
 
 const HomePage = () => {
 	const {
@@ -17,12 +18,26 @@ const HomePage = () => {
 	} = useMusicStore();
 
 	const { initializeQueue } = usePlayerStore();
+	const { user } = useUser();
+	const [greeting, setGreeting] = useState("");
+	const [greetingEmoji, setGreetingEmoji] = useState<string>("");
 
 	useEffect(() => {
-		fetchFeaturedSongs();
-		fetchMadeForYouSongs();
-		fetchTrendingSongs();
-	}, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+		const getGreeting = () => {
+			const hour = new Date().getHours();
+			if (hour >= 0 && hour < 13) {
+				setGreetingEmoji("☀️");
+				return "Good morning";
+			} else if (hour >= 13 && hour < 17) {
+				setGreetingEmoji("🌤️");
+				return "Good afternoon";
+			} else {
+				setGreetingEmoji("🌙");
+				return "Good evening";
+			}
+		};
+		setGreeting(getGreeting());
+	}, []);
 
 	useEffect(() => {
 		if (madeForYouSongs.length > 0 && featuredSongs.length > 0 && trendingSongs.length > 0) {
@@ -35,7 +50,10 @@ const HomePage = () => {
 		<main className='rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
 			<ScrollArea className='h-[calc(100vh-180px)]'>
 				<div className='p-4 sm:p-6'>
-					<h1 className='text-2xl sm:text-3xl font-bold mb-6'>Good afternoon</h1>
+					<h1 className='text-2xl sm:text-3xl font-bold mb-6'>
+						{greeting}
+						{user?.username ? ` ${user.username}!` : "!"} {greetingEmoji}
+					</h1>
 					<FeaturedSection />
 
 					<div className='space-y-8'>
