0fbbd1e15da9f51bfd25320d8b7e17a2a316d445
kontses <libys97@gmail.com>
2025-06-10 06:27:24 +0300

    Αφαίρεση SectionGrid από τη σελίδα αρχικής σελίδας.

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -2,7 +2,6 @@ import { useMusicStore } from "@/stores/useMusicStore";
 import { useEffect, useState } from "react";
 import FeaturedSection from "./components/FeaturedSection";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import SectionGrid from "./components/SectionGrid";
 import { usePlayerStore } from "@/stores/usePlayerStore";
 import { useUser } from "@clerk/clerk-react";
 import { useRecentPlaysStore } from "@/stores/useRecentPlaysStore";
